5c06b145a21285d6c562c22622ca1b8940949253
Auwal <engr.auwal@gmail.com>
2020-06-08 23:37:46 +0100

    Add project files.

---
+++ AIHumanResource/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AIHumanResource.MvcApplication" Language="C#" %>
---
+++ AIHumanResource/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AIHumanResource.Startup))]
+namespace AIHumanResource
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
